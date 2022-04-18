f1362698225ef7c86b1f0c3cec93fd035c16e34b
David Rodríguez Elipe <egoz3ntrum@gmail.com>
2022-04-18 17:00:51 +0200

    20220418170051

--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-export WP_CLI_CACHE_DIR=/var/www/html/.wp-cli/cache
 set -ex
-sleep 3
-ls -I entrypoint.sh | xargs rm -rf &> /dev/null
-wp core download --path="/var/www/html" --locale="es_ES"
-wp core config --force --path="/var/www/html" --dbuser=$MSQL_ADMIN  \
-    --dbpass="${WP_PASS}" --dbhost="$DB_HOST" --dbname="$DB_NAME" \
-    --dbprefix="wp_" --skip-check 
-
-wp core install\
-    --path="/var/www/html" \
-    --url=${WP_URL}\
-    --title=${WP_TITLE}\
-    --admin_name=${WP_ADMIN}\
-    --admin_password=${WP_PASS}\
-    --admin_email=${WP_MAIL} 
-wp core language install es_ES --activate --path="/var/www/html"
-wp user create ${WP_ADMIN} ${WP_PASS} --role=administrator
+# wp core download --path="/var/www/html/" --locale="es_ES"
+# wp core config --path="/var/www/html/" --dbuser=mysql  \
+#     --dbpass="${MSQL_PW}" --dbhost="${DB_HOST}" --dbname="${DB_NAME}" \
+#     --dbprefix="wp_" --skip-check
+sleep 5
+wp core install --path="/var/www/html/" --url="darodrig.42.fr" \
+    --title=${WP_TITLE} --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" \
+    --admin_email="$WP_MAIL" --skip-email
 /usr/sbin/php-fpm7.3 --nodaemonize
-tail -f
+# tail -f
