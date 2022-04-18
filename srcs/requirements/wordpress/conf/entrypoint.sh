4b9d272662190bfc79ee739c886fd5c05dba351f
d-r-e <d-r-e>
2022-04-18 22:03:06 +0200

    20220418220306

--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -3,12 +3,17 @@
 set -e
 
 until nc -z mariadb 3306; do
-	echo  "Waiting for database connection..."
+	echo  "\rWaiting for database connection..."
   sleep 1
 done
+
+
 wp core install --path="/var/www/html/" --url="darodrig.42.fr" \
     --title=${WP_TITLE} --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" \
     --admin_email="$WP_MAIL" --skip-email
+
+echo "WordPress configured"
+
 /usr/sbin/php-fpm7.3 --nodaemonize 
 
 # tail -f
