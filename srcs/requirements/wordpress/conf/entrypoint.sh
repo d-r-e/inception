44c3e739ab528cdd93dc6924945a41f29e7cb096
David Rodríguez Elipe <egoz3ntrum@gmail.com>
2022-04-21 18:39:56 +0200

    20220421183956

--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -3,16 +3,16 @@
 set -e
 
 until nc -z mariadb 3306; do
-	echo  "\rWaiting for database connection..."
+	echo  "Waiting for database connection..."
   sleep 1
 done
 
 if [ ! -f /var/www/html/wp-config.php ]; then
   echo "Creating wordpress database"
-
+  # rm -rf $(ls -A /var/www/html/ | grep -v wp-config.php)
   wp config create --path="/var/www/html/" --dbname="${DB_NAME}" \
   --dbuser="${MSQL_ADMIN}" --dbpass="${MSQL_PW}" --dbhost="${DB_HOST}" --dbprefix="wp_" --locale="es_ES"
-
+  sleep 1
   wp core install --path="/var/www/html/" --url="${WP_URL}" \
       --title=${WP_TITLE} --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" \
       --admin_email="$WP_MAIL" --skip-email
@@ -20,6 +20,6 @@ if [ ! -f /var/www/html/wp-config.php ]; then
   echo "WordPress configured"
 fi
 
-/usr/sbin/php-fpm7.3 --nodaemonize 
+/usr/sbin/php-fpm7.3 --nodaemonize
 
 # tail -f
