a44f678fbc05a41998c725ee9ed45924d9d062f8
David Rodríguez Elipe <egoz3ntrum@gmail.com>
2022-04-22 22:13:16 +0200

    20220422221315

--- srcs/requirements/autoheal/conf/autoheal.sh
+++ srcs/requirements/autoheal/conf/autoheal.sh
@@ -6,13 +6,26 @@ DOCKER_ENDPOINT=url="${HTTP_ENDPOINT}/containers/json?filters=\{\"health\":\[\"u
 ## Minimalist version from the fantastic Autoheal from Will Farrell
 
 get_unhealthy_containers() {
-  curl -XGET -s --unix-socket /var/run/docker.sock "http://localhost/containers/json?filters=\{\"health\":\[\"unhealthy\"\]${label_filter}\}" | jq .
+  # curl -s -XGET --unix-socket /var/run/docker.sock "http://localhost/containers/json?filters=\{\"health\":\[\"unhealthy\"\]\}" | jq .
+  curl -s -XGET --unix-socket /var/run/docker.sock "http://localhost/containers/json?filters=\{\"health\":\[\"unhealthy\"\]\}" 
 }
+echo Autoheal is watching...
+
 # tail -f &>/dev/null
 while true; do
-    sleep 3
-    json=$(get_unhealthy_containers)
-    echo $json
+    sleep $AUTOHEAL_INTERVAL
+    get_unhealthy_containers | jq -r '.[].Names' | while read dead; do
+      dead=$(echo $dead | sed 's/\///g' | sed 's/\"//g' | sed 's/\[//g' | sed 's/\]//g' | tr -d ' ')
+      ## if dead length is greater than 1, then it's a container name
+      if [ ${#dead} -gt 1 ]; then
+        echo "Restarting container $dead"
+        # docker restart $dead
+        curl -s -X POST --unix-socket /var/run/docker.sock "http://localhost/containers/$dead/restart"
+      fi
+      # echo Trying to restart $dead
+    done
 done
 
+
 #curl -XGET --unix-socket /var/run/docker.sock "http://localhost/containers/json?filters=\{\"health\":\[\"unhealthy\"\]${label_filter}\}"
+
