b2bb0abd67685a420dd4cfed3457f3cfd203e7d6
David Rodríguez Elipe <egoz3ntrum@gmail.com>
2022-04-24 12:45:16 +0200

    20220424124516

--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -7,21 +7,21 @@ until nc -z mariadb 3306; do
   sleep 1
 done
 
-if [ ! -f /var/www/html/wp-config.php ]; then
+# If not configured
+if [ ! -f /var/www/html/wp-config.php || ! -f /var/www/html/.configured ]; then
   echo "Creating wordpress database"
-  # rm -rf $(ls -A /var/www/html/ | grep -v wp-config.php)
-  wp config create --path="/var/www/html/" --dbname="${DB_NAME}" \
-  --dbuser="${MSQL_ADMIN}" --dbpass="${MSQL_PW}" --dbhost="${DB_HOST}" --dbprefix="wp_" --locale="es_ES"
+  wp config create --path="/var/www/html/" \
+  --dbname="${DB_NAME}" \
+  --dbuser="${MSQL_ADMIN}" --dbpass="${MSQL_PW}" \
+  --dbhost="${DB_HOST}" --dbprefix="wp_" --locale="es_ES"
   sleep 1
   wp core install --path="/var/www/html/" --url="${WP_URL}" \
       --title=${WP_TITLE} --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" \
       --admin_email="$WP_MAIL" --skip-email
-  ## add regular user to wp
-  wp user create marvin marvin@42madrid.com --role=editor --user_pass=${WP_PASS} 
-
+  wp user create marvin marvin@42madrid.com --role=editor --user_pass=${WP_PASS}
   echo "WordPress configured"
+  touch /var/www/html/.configured
 fi
 
+# CMD
 /usr/sbin/php-fpm7.3 --nodaemonize
-
-# tail -f
